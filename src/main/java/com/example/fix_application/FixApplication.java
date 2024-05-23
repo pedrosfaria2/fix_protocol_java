dd8c2c9297b3a887547e8e69df25cedb57ecce1c
Pedro Serrano Faria <pedroserrano2@gmail.com>
2024-05-23 20:28:13 -0300

    initial commit

---
+++ src/main/java/com/example/fix_application/FixApplication.java
@@ -0,0 +1,13 @@
+package com.example.fix_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fix_application/FixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fix_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
